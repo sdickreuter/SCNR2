0e7eb451f984c97a8a6745db8bec577481491497
diggiharry <s-l-u-r-p@gmx.de>
2016-07-17 19:35:49 +0200

    trying out new Ansatz with multiprocess and shared memory

--- launcher.py
+++ launcher.py
@@ -69,7 +69,7 @@ class SpectrometerProcess(StatusProcess):
                 raise KeyboardInterrupt()
 
             elif msg == 'test':
-                self.spec[0] = param
+                self.spec[0] += param
                 self.send_status('ok')
 
             elif msg == 'getwidth':
@@ -142,7 +142,6 @@ class SpectrometerProcess(StatusProcess):
 
 
 class SpectrometerController:
-    count = 0
 
     def __init__(self, status_queue, command_queue):
         self.status = status_queue
@@ -154,8 +153,7 @@ class SpectrometerController:
         return status
 
     def test(self):
-        self.count += 1
-        return self.make_request('test',self.count)
+        return self.make_request('test',1)
 
     def GetWidth(self):
         return self.make_request('getwidth', None)
@@ -283,23 +281,17 @@ if __name__ == '__main__':
     com2 = mp.Queue()
 
     N = 10
-    spec_shared = mp.Array(ctypes.c_double, N)
-    spec = np.ctypeslib.as_array(spec_shared.get_obj())
-    #spec = spec.reshape(10)
+    spec = mp.Array(ctypes.c_double, N)
 
     img_shared = mp.Array(ctypes.c_double, N*N)
-    img = np.ctypeslib.as_array(img_shared.get_obj())
-    img = img.reshape(N,N)
+    #img = np.ctypeslib.as_array(img_shared.get_obj())
+    #img = img.reshape(N,N)
 
-    proc1 = SpectrometerProcess(status1,com1,spec_shared)
+    proc1 = SpectrometerProcess(status1,com1,spec)
     proc1.daemon = True
     proc1.start()
 
-    #cont = SpectrometerController(status1,com1)
-
-    proc2 = GuiProcess(status1,com1,spec_shared)
-    proc2.daemon = True
-    #proc2.start()
+    proc2 = GuiProcess(status1, com1, spec)
 
     count = 0
     try:
@@ -312,7 +304,8 @@ if __name__ == '__main__':
             #time.sleep(1)
 
             if not proc2.is_alive():
-                proc2 = GuiProcess(status1, com1,spec_shared)
+                proc2 = GuiProcess(status1, com1,spec)
+                proc2.daemon = True
                 proc2.start()
             # else:
             #     try:
