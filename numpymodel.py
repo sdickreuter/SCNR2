ab786f8b56e263926bfc5068a41eca5276f6ef17
sdickreuter <Simon.Dickreuter@uni-tuebingen.de>
2017-02-03 11:39:04 +0100

    trying to fix numpymodel

--- numpymodel.py
+++ numpymodel.py
@@ -1,41 +1,83 @@
 from PyQt5.QtCore import QAbstractTableModel, QVariant, Qt
+import numpy as np
+
+# class NumpyModel(QAbstractTableModel):
+#     def __init__(self, nmatrix, parent=None):
+#         QAbstractTableModel.__init__(self, parent)
+#         self._matrix = nmatrix
+#
+#     def update(self, narray):
+#         self._matrix = narray.copy()
+#         self.layoutChanged.emit()
+#
+#     def getMatrix(self):
+#         return self._matrix.copy()
+#
+#     def rowCount(self, parent=None):
+#         return self._matrix.shape[0]
+#
+#     def columnCount(self, parent=None):
+#         return self._matrix.shape[1]
+#
+#     def data(self, index, role=Qt.DisplayRole):
+#         if index.isValid():
+#             if role == Qt.DisplayRole or role == Qt.EditRole :
+#                 row = index.row()
+#                 col = index.column()
+#                 return QVariant("%.5f"%self._matrix[row, col])
+#         return QVariant()
+#
+#     def setData(self, index, value, role = Qt.EditRole):
+#         if role == Qt.EditRole:
+#             try:
+#                 val = float(value)
+#             except:
+#                 return False
+#             self._matrix[index.row(),index.column()] = val
+#             self.dataChanged.emit(index, index, ())
+#             return True
+#         return False
+#
+#     def flags(self, index):
+#         #if (index.column() == 0):
+#         return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+#         #else:
+#         #    return Qt.ItemIsEnabled
+#
+#     def headerData(self, col, orientation, role):
+#         if role != Qt.DisplayRole:
+#             return QVariant()
+#
+#         if orientation == Qt.Horizontal:
+#             if col == 0:
+#                 return 'x'
+#             if col == 1:
+#                 return 'y'
+#         return QVariant()
+
+
 
 class NumpyModel(QAbstractTableModel):
-    def __init__(self, nmatrix, parent=None):
+    """
+    Class to populate a table view with a pandas dataframe
+    """
+    def __init__(self, data, parent=None):
         QAbstractTableModel.__init__(self, parent)
-        self._matrix = nmatrix
-
-    def update(self, narray):
-        self._matrix = narray.copy()
-        self.layoutChanged.emit()
-
-    def getMatrix(self):
-        return self._matrix.copy()
+        self._data = np.array(data.values)
+        self._cols = data.columns
+        self.r, self.c = np.shape(self._data)
 
     def rowCount(self, parent=None):
-        return self._matrix.shape[0]
+        return self.r
 
     def columnCount(self, parent=None):
-        return self._matrix.shape[1]
+        return self.c
 
     def data(self, index, role=Qt.DisplayRole):
         if index.isValid():
-            if role == Qt.DisplayRole or role == Qt.EditRole :
-                row = index.row()
-                col = index.column()
-                return QVariant("%.5f"%self._matrix[row, col])
-        return QVariant()
-
-    def setData(self, index, value, role = Qt.EditRole):
-        if role == Qt.EditRole:
-            try:
-                val = float(value)
-            except:
-                return False
-            self._matrix[index.row(),index.column()] = val
-            self.dataChanged.emit(index, index, ())
-            return True
-        return False
+            if role == Qt.DisplayRole:
+                return self._data[index.row(),index.column()]
+        return None
 
     def flags(self, index):
         #if (index.column() == 0):
